b137317dce2f8dc5ac7038ba94d40b5efd13ae0e
IbnuJabir <kedirjabir12@gmail.com>
2024-12-14 10:31:51 +0300

    style: add ui loader

---
+++ apps/web-trello/src/components/loader.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import { LineWave } from "react-loader-spinner";
+import ScaleLoader from "react-spinners/ScaleLoader";
+function Loader() {
+  return <ScaleLoader color="#fff" height={20} width={3} />;
+}
+
+export default Loader;
